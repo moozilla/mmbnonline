4c701b653664ecf894d507940ba80f2be3e6aac7
evilgeniuscow <evilgeniuscow@8bee0d91-8a1e-0410-b811-83a365e83f77>
2006-10-18 06:25:02 +0000

    No updates to the main program, but I'm including a Work In Progress map editor.

---
+++ trunk/map/mapED/mapED/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿/*
+ * (Program Name Here)
+ * 
+ * Created: 10/17/2006 at 6:03 PM
+ * 
+ */
+namespace mapED
+{
+	partial class MainForm : System.Windows.Forms.Form
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pTiles = new System.Windows.Forms.Panel();
+			this.gTiles = new System.Windows.Forms.GroupBox();
+			this.tileScroll = new System.Windows.Forms.HScrollBar();
+			this.openTiles = new System.Windows.Forms.OpenFileDialog();
+			this.bLoadTiles = new System.Windows.Forms.Button();
+			this.bLoadMap = new System.Windows.Forms.Button();
+			this.gMap = new System.Windows.Forms.GroupBox();
+			this.pMapScroll = new System.Windows.Forms.Panel();
+			this.pMap = new System.Windows.Forms.Panel();
+			this.gMapData = new System.Windows.Forms.GroupBox();
+			this.tMapData = new System.Windows.Forms.TextBox();
+			this.openMap = new System.Windows.Forms.OpenFileDialog();
+			this.bGenMapData = new System.Windows.Forms.Button();
+			this.gTiles.SuspendLayout();
+			this.gMap.SuspendLayout();
+			this.pMapScroll.SuspendLayout();
+			this.gMapData.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pTiles
+			// 
+			this.pTiles.BackColor = System.Drawing.Color.Fuchsia;
+			this.pTiles.Location = new System.Drawing.Point(6, 20);
+			this.pTiles.Name = "pTiles";
+			this.pTiles.Size = new System.Drawing.Size(304, 8);
+			this.pTiles.TabIndex = 0;
+			// 
+			// gTiles
+			// 
+			this.gTiles.Controls.Add(this.tileScroll);
+			this.gTiles.Controls.Add(this.pTiles);
+			this.gTiles.Location = new System.Drawing.Point(12, 12);
+			this.gTiles.Name = "gTiles";
+			this.gTiles.Size = new System.Drawing.Size(318, 46);
+			this.gTiles.TabIndex = 1;
+			this.gTiles.TabStop = false;
+			this.gTiles.Text = "Tiles";
+			// 
+			// tileScroll
+			// 
+			this.tileScroll.Location = new System.Drawing.Point(6, 32);
+			this.tileScroll.Name = "tileScroll";
+			this.tileScroll.Size = new System.Drawing.Size(304, 10);
+			this.tileScroll.TabIndex = 1;
+			this.tileScroll.Scroll += new System.Windows.Forms.ScrollEventHandler(this.TileScrollScroll);
+			// 
+			// openTiles
+			// 
+			this.openTiles.Filter = "PNG Images|*.PNG|All Files|*.*";
+			// 
+			// bLoadTiles
+			// 
+			this.bLoadTiles.Location = new System.Drawing.Point(12, 64);
+			this.bLoadTiles.Name = "bLoadTiles";
+			this.bLoadTiles.Size = new System.Drawing.Size(75, 23);
+			this.bLoadTiles.TabIndex = 2;
+			this.bLoadTiles.Text = "Load Tiles";
+			this.bLoadTiles.UseVisualStyleBackColor = true;
+			this.bLoadTiles.Click += new System.EventHandler(this.BLoadTilesClick);
+			// 
+			// bLoadMap
+			// 
+			this.bLoadMap.Location = new System.Drawing.Point(93, 64);
+			this.bLoadMap.Name = "bLoadMap";
+			this.bLoadMap.Size = new System.Drawing.Size(96, 23);
+			this.bLoadMap.TabIndex = 3;
+			this.bLoadMap.Text = "Load Map Image";
+			this.bLoadMap.UseVisualStyleBackColor = true;
+			this.bLoadMap.Click += new System.EventHandler(this.BLoadMapClick);
+			// 
+			// gMap
+			// 
+			this.gMap.Controls.Add(this.pMapScroll);
+			this.gMap.Location = new System.Drawing.Point(12, 93);
+			this.gMap.Name = "gMap";
+			this.gMap.Size = new System.Drawing.Size(177, 128);
+			this.gMap.TabIndex = 4;
+			this.gMap.TabStop = false;
+			this.gMap.Text = "Map Image";
+			// 
+			// pMapScroll
+			// 
+			this.pMapScroll.AutoScroll = true;
+			this.pMapScroll.AutoScrollMinSize = new System.Drawing.Size(155, 92);
+			this.pMapScroll.Controls.Add(this.pMap);
+			this.pMapScroll.Location = new System.Drawing.Point(6, 20);
+			this.pMapScroll.Name = "pMapScroll";
+			this.pMapScroll.Size = new System.Drawing.Size(165, 102);
+			this.pMapScroll.TabIndex = 1;
+			// 
+			// pMap
+			// 
+			this.pMap.BackColor = System.Drawing.Color.Fuchsia;
+			this.pMap.Location = new System.Drawing.Point(3, 3);
+			this.pMap.Name = "pMap";
+			this.pMap.Size = new System.Drawing.Size(159, 96);
+			this.pMap.TabIndex = 0;
+			// 
+			// gMapData
+			// 
+			this.gMapData.Controls.Add(this.bGenMapData);
+			this.gMapData.Controls.Add(this.tMapData);
+			this.gMapData.Location = new System.Drawing.Point(195, 64);
+			this.gMapData.Name = "gMapData";
+			this.gMapData.Size = new System.Drawing.Size(135, 157);
+			this.gMapData.TabIndex = 5;
+			this.gMapData.TabStop = false;
+			this.gMapData.Text = "Map Data";
+			// 
+			// tMapData
+			// 
+			this.tMapData.Location = new System.Drawing.Point(6, 20);
+			this.tMapData.Multiline = true;
+			this.tMapData.Name = "tMapData";
+			this.tMapData.Size = new System.Drawing.Size(121, 102);
+			this.tMapData.TabIndex = 0;
+			// 
+			// openMap
+			// 
+			this.openMap.Filter = "PNG Images|*.PNG|All Files|*.*";
+			// 
+			// bGenMapData
+			// 
+			this.bGenMapData.Location = new System.Drawing.Point(6, 128);
+			this.bGenMapData.Name = "bGenMapData";
+			this.bGenMapData.Size = new System.Drawing.Size(121, 23);
+			this.bGenMapData.TabIndex = 1;
+			this.bGenMapData.Text = "Generate";
+			this.bGenMapData.UseVisualStyleBackColor = true;
+			this.bGenMapData.Click += new System.EventHandler(this.BGenMapDataClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(342, 233);
+			this.Controls.Add(this.gMapData);
+			this.Controls.Add(this.gMap);
+			this.Controls.Add(this.bLoadMap);
+			this.Controls.Add(this.bLoadTiles);
+			this.Controls.Add(this.gTiles);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "MainForm";
+			this.Text = "mapED";
+			this.gTiles.ResumeLayout(false);
+			this.gMap.ResumeLayout(false);
+			this.pMapScroll.ResumeLayout(false);
+			this.gMapData.ResumeLayout(false);
+			this.gMapData.PerformLayout();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button bGenMapData;
+		private System.Windows.Forms.Panel pMapScroll;
+		private System.Windows.Forms.OpenFileDialog openMap;
+		private System.Windows.Forms.TextBox tMapData;
+		private System.Windows.Forms.GroupBox gMapData;
+		private System.Windows.Forms.Panel pMap;
+		private System.Windows.Forms.GroupBox gMap;
+		private System.Windows.Forms.Button bLoadMap;
+		private System.Windows.Forms.HScrollBar tileScroll;
+		private System.Windows.Forms.Button bLoadTiles;
+		private System.Windows.Forms.OpenFileDialog openTiles;
+		private System.Windows.Forms.GroupBox gTiles;
+		private System.Windows.Forms.Panel pTiles;
+	}
+}
---
+++ trunk/map/mapED/mapED/MainForm.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * (Program Name Here)
+ * 
+ * Created: 10/17/2006 at 6:03 PM
+ * 
+ */
+
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace mapED
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm
+	{
+		Collection<Image> tiles = new Collection<Image>();
+		int tileOffset = 0;
+		
+		bool tilesLoaded = false;
+		bool mapLoaded = false;
+		
+		Image mapImage;
+		
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+		}
+		
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			if(tilesLoaded) updateTiles();
+		}
+		
+		void BLoadTilesClick(object sender, System.EventArgs e)
+		{
+			if(openTiles.ShowDialog()==DialogResult.OK) {
+				Image temp = Image.FromFile(openTiles.FileName);
+				for(int i=0;i<temp.Width;i+=8) {
+					Image tmp = new Bitmap(8,8);
+					Graphics.FromImage(tmp).DrawImage(temp,new Rectangle(0,0,8,8),new Rectangle(i,0,8,8), GraphicsUnit.Pixel);
+					tiles.Add(tmp);
+				}
+				if(tiles.Count>29) {
+					tileScroll.Maximum = tiles.Count-29;
+				} else {
+					tileScroll.Enabled = false;
+				}
+				tilesLoaded = true;
+				updateTiles();
+			}
+		}
+		
+		void updateTiles()
+		{
+			Graphics g = pTiles.CreateGraphics();
+			g.Clear(pTiles.BackColor);
+			for(int i=0;i<38;i++) {
+				g.DrawImage(tiles[i+tileOffset],8*i,0,8,8);
+			}
+			g.Dispose();
+		}
+		
+		void TileScrollScroll(object sender, System.Windows.Forms.ScrollEventArgs e)
+		{
+			tileOffset = tileScroll.Value;
+			updateTiles();
+		}
+		
+		void BLoadMapClick(object sender, System.EventArgs e)
+		{
+			if(openMap.ShowDialog()==DialogResult.OK)
+			{
+				mapImage = Image.FromFile(openMap.FileName);
+				pMap.BackgroundImage = mapImage;
+				pMap.Size = mapImage.Size;
+				mapLoaded = true;
+			}
+		}
+		
+		void BGenMapDataClick(object sender, System.EventArgs e)
+		{
+			if(tilesLoaded&&mapLoaded)
+			{
+				for(int i=0;i<mapImage.Width;i+=8) {
+					Image tmp = new Bitmap(8,8);
+					Graphics.FromImage(tmp).DrawImage(mapImage,new Rectangle(0,0,8,8),new Rectangle(i,0,8,8), GraphicsUnit.Pixel); //piece of map image
+					int idx = tiles.IndexOf(tmp);
+					if(idx==-1) {
+						Image temp = (Image)tmp.Clone();
+						System.Diagnostics.Trace.WriteLine(tmp.PropertyItems==temp.PropertyItems);
+						//tmp.Save(Application.StartupPath + "\\test"+i+".bmp");
+						//MessageBox.Show("Piece not found in tiles.","Bad tiles", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						//return;
+					}
+					tMapData.Text+=idx+",";
+				}
+			} else {
+				MessageBox.Show("Please load both the tile and the map image.","Load images", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+			}
+		}
+	}
+}
