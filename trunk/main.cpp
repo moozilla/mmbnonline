a33f3bd379a1d0a3e7ca207851a2e5e7717fb2e0
nareion <nareion@8bee0d91-8a1e-0410-b811-83a365e83f77>
2007-06-15 03:13:59 +0000

    This is the new SDL/C++ version of MMBNO. All files needed to compile and run are here. The files called "Nare test" are just for using CodeBlocks to compile.

---
+++ trunk/main.cpp
@@ -0,0 +1,155 @@
+#include <cstdlib>
+#include <direct.h> // for getcwd
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "navi.h"
+#include "Timer.h"
+#include "SDL_Func.h"
+/*
+ * MMBNO Main
+ *
+ * Created: 3/18/2007 at 2:00 PM by Nare
+ *
+ */
+int main ( int argc, char** argv )
+{
+    // initialize SDL video
+ if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "Unable to init SDL: %s\n", SDL_GetError() );
+        return 1;
+    }
+
+    // make sure SDL cleans up before exit
+    atexit(SDL_Quit);
+
+    // create a new window
+    SDL_Surface* screen = SDL_SetVideoMode(800, 600, 32,
+                                           SDL_HWSURFACE|SDL_DOUBLEBUF);
+    if ( !screen )
+    {
+        printf("Unable to set 640x480 video: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    //This 4 lines are for getting the program path
+    char buffer[_MAX_PATH]; //make an array of chars to save the program path
+    string programPath; //where the path WILL be saved
+    getcwd(buffer, _MAX_PATH); //gets the program path
+    programPath=buffer; //saves the path in the string
+
+    // load a navi
+
+    Navi a(programPath + "\\skin.txt");
+
+    //this is the original map, we will always use it as a base for the screen
+    SDL_Surface* map;
+    SDL_Surface* optimizebuffer;
+    optimizebuffer = IMG_Load("map.png");
+    map = SDL_DisplayFormat(optimizebuffer);
+    SDL_FreeSurface(optimizebuffer);
+
+    bool done = false; //this says that it has to do
+    SDL_Rect background;
+    background.x=(screen->w - map->w)/2; //centers the background horizontally
+    background.y=(screen->h - map->h)/2; //centers the background vertically
+    a.move((screen->w - a.naviWidth)/2,(screen->h - 2*a.naviHeight)/2,0,true, true); //puts our little guy STANDING on the center of the screen
+    int offsetx=0; //nuff said
+    int offsety=0; //nuff said
+    int direction=0; //0 facing, 1 right-down 2 right, 3 right-up, 4 back
+    Timer fps; //This little thingy lets me keep track of framerating
+    bool standing=true; //nuff said
+   // map=flip_surface(map, FLIP_HORIZONTAL);
+
+    // program main loop
+    while (!done)
+    {
+        fps.start(); //strat the counter for framerating
+
+        // message processing loop
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            // check for messages
+            switch (event.type)
+            {
+                // exit if the window is closed
+            case SDL_QUIT:
+                done = true;
+                break;
+
+                // check for keypresses
+            case SDL_KEYDOWN:
+
+                    // exit if ESCAPE is pressed
+                    switch(event.key.keysym.sym)
+                    {
+                        case SDLK_DOWN:standing=false; offsety=8;break;
+                        case SDLK_UP:standing=false; offsety=-8;break;
+                        case SDLK_LEFT:standing=false; offsetx=-8;break;
+                        case SDLK_RIGHT:standing=false; offsetx=8;break;
+                        case SDLK_ESCAPE:standing=false; done = true;break;
+                    }
+                break;
+            case SDL_KEYUP:
+
+                    switch(event.key.keysym.sym)
+                    {
+                        case SDLK_DOWN: offsety=0;break;
+                        case SDLK_UP: offsety=0;break;
+                        case SDLK_LEFT: offsetx=0;break;
+                        case SDLK_RIGHT: offsetx=0;break;
+                    }
+            break;
+            } // end switch
+        } // end of message processing
+
+      if(offsetx==0){
+          switch(offsety){
+            case 0: standing=true;break; //isn't moving
+            case 8: direction=0;break; //is going down
+            case -8: direction=4;break; //is going up
+          }
+      }
+      else{
+        if(offsetx==8){ //goes right
+            switch(offsety){
+                case 8:offsety=4;direction=1;break; //is going right-down
+                case 0:direction=2;break; //is going right
+                case -8:offsety=-4;direction=3;break; //is going right-up
+            }
+        }
+        else{ //goes left
+            switch(offsety){
+                case 8:offsety=4;direction=5;break; //is going left-down
+                case 0:direction=6;break; //is going left
+                case -8:offsety=-4;direction=7;break; //is going left-up
+            }
+        }
+      }
+
+  //      *screen = *map;
+        // DRAWING STARTS HERE
+
+        // clear screen
+        SDL_BlitSurface(map, 0, screen, &background); //draw the map, that would CLEAR THE SCREEN
+        a.move(offsetx,offsety,direction,true, standing); //we move the navi
+        a.draw(screen); //we DRAW the navi on the screen
+        // DRAWING ENDS HERE
+
+
+        //Limit the framerate
+            while( fps.get_ticks() < 1000 / 24 )
+            {
+                 //wait...
+            }
+
+
+        // finally, update the screen :)
+        SDL_Flip(screen);
+    } // end main loop
+
+    // all is well ;)
+    printf("Exited cleanly\n");
+    return 0;
+}
---
+++ trunk/navi.h
@@ -0,0 +1,176 @@
+#ifndef NAVI_H_INCLUDED
+#define NAVI_H_INCLUDED
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <string>
+#include <fstream>
+#include <map>
+#include "SDL_Func.h"
+/*
+ * MMBNO navi class
+ *
+ * Created: 10/6/2006 at 9:04 PM by Spikeman
+ *
+ */
+/*
+ * Edited: 10/10/2006 at 2:15 AM by Nare
+ *
+ * Changed the contructor to parse the skin file automatically por the navi data
+ *
+ * Edited: 3/18/2007 at 2:05 PM by Nare
+ *
+ * Migrated to C++
+ *
+ * Edited: 5/14/2007 at 2:05 PM by Nare
+ *
+ * finished migration to C++/SDL
+ - ONLY THING LEFT TO DO IS THE H FLIPPING
+
+ * Edited: 6/14/2007 at 10:00 PM by Nare
+ *
+ * Flipping Works!!
+
+	/// <summary>
+	/// The class for navis.
+	/// </summary>
+*/
+using namespace std;
+
+	class Navi
+	{
+		private:
+            int naviX; //X position in the map
+            int naviY; //Y position in the map
+            int naviDir; //Direction the navi is facing
+            int naviFrame; //number of frame that should be shown
+            bool naviHFlip; //Stores if the navi need HFlipping
+            int naviNumFrames; //amount of frames in the spritesheet
+            int naviStandingFrames; //1 if stnads still, ammount of frames if moves while standing
+            SDL_Surface* naviImage; //the spritesheet itself
+            SDL_Surface* todraw; //the frame to draw
+
+
+public:
+  bool loadNavi(string filename);
+  Navi(string filename); //full filename
+  ~Navi(void);
+  int naviWidth;
+  int naviHeight;
+  void draw(SDL_Surface* g); //This is the
+  bool IsFlipped(void); //Returns if the navi uses the flipping process or is stored in spritesheet
+  void FlipH(void); //Changes the naviHflip variable
+  void move(int x, int y, int direction, bool nextframe, bool stand);
+
+};
+
+
+
+bool Navi::IsFlipped(void)
+{
+			return naviHFlip;
+}
+
+void Navi::FlipH(void)
+{
+    naviHFlip = !naviHFlip;
+}
+
+Navi::Navi(string filename) //full filename
+{
+    naviX=0;
+    naviY=0;
+    naviDir=0;
+    naviFrame=0;
+	if(!loadNavi(filename)){/*	throw new System.Exception("Invalid skin");*/}
+}
+
+Navi::~Navi(void)
+{
+}
+
+bool Navi::loadNavi(string filename)
+{
+			//parses file into a dictionary so anything later added to skins can be added very easily
+
+	ifstream archivo;
+	string line;
+	string aux;
+	map<string, string> vals; //store data from file in dictionary
+        archivo.open(filename.c_str());
+        getline(archivo,line);
+        int i;
+	while (!(archivo.eof())) {
+		if(line.find("//")!=0) //only gets lines that aren't comments
+		{
+			int t = line.find("=");
+			aux=line.substr(0,t);
+//THIS FOR IS HERE BECAUSE THE DARNED GCC COMPILER HAS A BUG WITH THE NORMAL WAY RO CONVERT A STRING TO LOWER CASE
+			for(i=0; i<aux.length();i++){aux[i]=tolower(aux[i]);}
+			string key = aux; //key name is on left of equal sign
+			aux=line.substr(t+1);
+			for(i=0; i<aux.length();i++){aux[i]=tolower(aux[i]);}
+			string val = aux; //value name on right of equal sign
+			vals[key]=val;
+		}
+		getline(archivo,line);
+	}
+	archivo.close();
+
+	string path = filename.substr(0,filename.find_last_of("\\"));
+
+	naviWidth = atoi(vals["width"].c_str());
+	naviHeight = atoi(vals["height"].c_str());
+	naviNumFrames = atoi(vals["frames"].c_str());
+	naviHFlip = atoi(vals["hflip"].c_str());
+	path = path + "\\" + vals["filename"].c_str();
+	naviImage = IMG_Load(path.c_str());
+	if(vals.find("standingframes")!=vals.end()){naviStandingFrames = atoi(vals["standingframes"].c_str());} else{naviStandingFrames = 1;}; //default 1
+	return true; //success
+}
+
+void Navi::draw(SDL_Surface* g)		{
+	SDL_Rect rectscr; //where it goes in the screen
+	SDL_Rect rectnavi; //where it has to take the frame from in the sprite sheet
+	SDL_Rect rectframe; //to blit the into todraw
+
+	rectscr.x=naviX;
+	rectscr.y=naviY;
+	rectscr.w=naviWidth;
+	rectscr.h=naviHeight;
+
+	rectnavi.x=naviFrame*naviWidth;
+	rectnavi.w=naviWidth;
+	rectnavi.h=naviHeight;
+
+	rectframe.x=0;
+	rectframe.y=0;
+	rectframe.w=naviWidth;
+	rectframe.h=naviHeight;
+	//if the Hflip is on, then we must flip the navi before drawing
+        //I initialize the cropped navi image
+	todraw = SDL_CreateRGBSurface(naviImage->flags, rectscr.w, rectscr.h, naviImage->format->BitsPerPixel, naviImage->format->Rmask, naviImage->format->Gmask, naviImage->format->Bmask, 0);
+	SDL_SetColorKey(todraw, SDL_SRCCOLORKEY,naviImage->format->colorkey);
+
+	if((naviDir>4) && naviHFlip){
+		rectnavi.y=(naviDir-4)*naviHeight; //I get the correct row
+		SDL_BlitSurface(naviImage,&rectnavi,todraw,&rectframe); //cut the one to flip
+		todraw=flip_surface(todraw, FLIP_HORIZONTAL); //and flip
+	}
+        else{
+		rectnavi.y=naviDir*naviHeight; //I get the correct row
+                SDL_BlitSurface(naviImage,&rectnavi,todraw,&rectframe); //if no need to flip, I just cut it
+	}
+	SDL_BlitSurface(todraw, &rectframe, g, &rectscr); //image,rect,frame*width,dir*height,width,height,GraphicsUnit.Pixel);
+}
+
+void Navi::move(int x, int y, int direction,bool nextframe, bool stand){
+    naviX+=x;
+    naviY+=y;
+    naviDir=direction;
+    if(nextframe){
+        naviFrame++;
+        if(naviFrame>naviNumFrames){naviFrame=0;}
+    }
+    if(stand){naviFrame=0;}
+}
+#endif // NEW_H_INCLUDED
